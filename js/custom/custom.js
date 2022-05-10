c246f4603747cdf0e83a3f387b576d615cac90e3
hjqecho <2454840488@qq.com>
2022-05-10 20:04:59 +0800

    Site updated: 2022-05-10 20:04:57

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -1,4 +1,4 @@
 /*添加图片top*/
-var top_up = "<img class='goup' src='https://npm.elemecdn.com/hjqecho_img/custom/goup.gif' title='回到顶部' >";
+var top_up = "<img class='gotop' src='https://npm.elemecdn.com/hjqecho_img/custom/goup.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
