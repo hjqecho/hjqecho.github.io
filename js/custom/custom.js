ee5f82af942e98369cb4dd61617b621532480116
hjqecho <2454840488@qq.com>
2022-06-04 20:15:30 +0800

    Site updated: 2022-06-04 20:15:26

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -1,5 +1,5 @@
 /*添加图片top*/
-var top_up = "<img class='gotop' src='https://npm.elemecdn.com/hjqecho_img/custom/goup.gif' title='回到顶部' >";
+var top_up = "<img class='gotop' src='https://npm.elemecdn.com/hjqecho_img@latest/custom/goup.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 
